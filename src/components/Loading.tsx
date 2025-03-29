37ecc4ac09e61f5cb3b9d41bd85a0a967d017953
Marcin Rundsztuk <marcinrundsztuk@Mac-mini-Marcin.local>
2025-03-29 18:04:41 +0100

    Add: loading screen component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,8 @@
+// src/components/Loading.tsx
+export default function Loading() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-gray-600 text-lg">Ładowanie...</p>
+      </div>
+    )
+  }
