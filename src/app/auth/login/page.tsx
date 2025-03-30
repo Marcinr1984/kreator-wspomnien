d9749db10997901ff3e5cc939caa31f31a720697
Marcin Rundsztuk <marcinrundsztuk@Mac-mini-Marcin.local>
2025-03-30 11:56:02 +0200

    Dopasowanie stylu strony logowania do rejestracji

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
-import { supabase } from '../../../../utils/supabaseClient'
+import { supabase } from '../../../utils/supabaseClient'
 
 export default function Login() {
   const router = useRouter()
