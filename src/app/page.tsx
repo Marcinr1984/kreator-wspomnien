6550d79cfc4c2cbbc6cc8045bb45939948aa7234
Marcin Rundsztuk <marcinrundsztuk@Mac-mini-Marcin.local>
2025-04-03 18:57:13 +0200

    Przekierowanie z / na /callback jeśli type=signup w URL hash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 'use client'
 
-import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter()
-
-  useEffect(() => {
-    const hash = window.location.hash.substring(1)
-    const params = new URLSearchParams(hash)
-    const type = params.get('type')
-
-    if (type === 'signup') {
-      router.push('/callback' + window.location.hash)
-    }
-  }, [router])
 
   return (
     <main className="p-4">
