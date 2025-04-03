c69a425bb6475e47aae0406e2f0a37596c8983f5
Marcin Rundsztuk <marcinrundsztuk@Mac-mini-Marcin.local>
2025-04-03 19:12:01 +0200

    Poprawka: pominięcie ekranu głównego i bezpośrednie przekierowanie do /callback po aktywacji konta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter()
+  const [shouldRender, setShouldRender] = useState(true)
 
   useEffect(() => {
     const hash = window.location.hash.substring(1)
@@ -12,9 +13,14 @@ export default function Home() {
     const type = params.get('type')
 
     if (type === 'signup') {
+      setShouldRender(false)
       router.push('/callback' + window.location.hash)
     }
   }, [router])
+
+  if (!shouldRender) {
+    return null
+  }
 
   return (
     <main className="p-4">
